e730d5ab6a3e07fb9db6b6149b30146b3672daa3
Jakub Legutko <kubanm3@gmail.com>
2020-12-26 17:02:24 +0100

    First version for users endpoint

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview, name="api-overview")
+    path('', views.apiOverview, name="api-overview"),
+    path('users/', views.users, name="users"),
+    path('user/<str:pk>/', views.user, name="user"),
+    path('user-create/', views.userCreate, name="user-create"),
+    path('user-update/<str:pk>/', views.userUpdate, name="user-update"),
+    path('user-delete/<str:pk>/', views.userDelete, name="user-delete"),
 ]
--- api/views.py
+++ api/views.py
@@ -1,5 +1,61 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import UserSerializer
+
+from .models import User
+
+
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse("API BASE POINT", safe=False)
+    api_urls = {
+        'List': '/users/',
+        'User details': '/user/<str:pk>/',
+        'Create user': '/user-create/',
+        'Update user': '/user-update/<str:pk>/',
+        'Delete user': '/user-delete/<str:pk>/',
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def users(request):
+    users = User.objects.all()
+    serializer = UserSerializer(users, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def user(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(user, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def userCreate(request):
+    serializer = UserSerializer(data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['PUT'])
+def userUpdate(request, pk):
+    user = User.objects.get(id=pk)
+    serializer = UserSerializer(instance=user, data=request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+@api_view(['DELETE'])
+def userDelete(request, pk):
+    user = User.objects.get(id=pk)
+    user.delete()
+
+    return Response('Item succsesfully delete!')
