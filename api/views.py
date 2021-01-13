056b30bbd69410eedfe5a85db239ade1adf851b7
Marek Szymański <marek.sz026@gmail.com>
2021-01-13 09:52:59 +0100

    Refactor api module

--- api/views.py
+++ api/views.py
@@ -6,14 +6,15 @@ from rest_framework.response import Response
 def apiOverview(request):
     api_urls = {
         'Users GET/POST': '/users/',
-        'User GET/PUT/DELETE': '/users/<str:pk>/',
-        'User Details GET/PUT': '/users/<str:pk>/details',
+        'User GET/PUT/DELETE': '/users/<str:pk>',
+        'User Login POST': '/user/login/',
+        'User Details GET/PUT': '/users/<str:pk>/details/',
         'DailyDiaries GET/POST': '/diaries/',
-        'DailyDiary GET/PUT/DELETE': '/diaries/<str:pk>/',
-        'Foods GET/POST': '/diaries/foods/all',
+        'DailyDiary GET/PUT/DELETE': '/diaries/<str:pk>',
+        'Foods GET/POST': '/diaries/foods/',
         'Foods for diary GET': '/diaries/<str:pk>/foods/',
         'Food GET/DELETE': '/diaries/foods/<str:pk>',
-        'Diaries by user': '/diaries/by-user/<str:pk>/',
+        'Diaries by user': '/diaries/by-user/<str:pk>',
         'Diary (today) by user': '/diaries/by-user/<str:pk>/today/',
     }
     return Response(api_urls)
